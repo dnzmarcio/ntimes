5c92971c2355c884ecf4ee753b40c254288f1c40
dnz.marcio@gmail.com <dnz.marcio@gmail.com>
2018-01-08 15:27:57 -0800

    Add nt_km.

--- R/utilities.R
+++ R/utilities.R
@@ -175,3 +175,77 @@ put_together <- function(descriptive.tab, test.tab, digits = 3,
 
   return(out)
 }
+
+#'Step ribbon statistic
+#'
+#'Provides stairstep values for ribbon plots
+#'
+#'@inheritParams ggplot2::geom_ribbon
+#'@param geom which geom to use; defaults to "`ribbon`"
+#'@param direction \code{hv} for horizontal-veritcal steps, `vh`` for
+#'   vertical-horizontal steps
+#'@references \url{https://groups.google.com/forum/?fromgroups=#!topic/ggplot2/9cFWHaH1CPs}
+#'@importFrom ggplot2 layer
+#'@export
+stat_stepribbon <- function(mapping=NULL, data=NULL, geom="ribbon",
+                            position="identity",
+                            na.rm=FALSE, show.legend=NA, inherit.aes=TRUE,
+                            direction="hv", ...) {
+
+  layer(
+    data = data,
+    mapping = mapping,
+    stat = StatStepribbon,
+    geom = geom,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      na.rm = na.rm,
+      direction = direction,
+      ...
+    )
+  )
+}
+
+#'ntimes-ggproto
+#'
+#'@format NULL
+#'@usage NULL
+#'@references \url{https://groups.google.com/forum/?fromgroups=#!topic/ggplot2/9cFWHaH1CPs}
+#'@importFrom ggplot2 ggproto
+#'@export
+StatStepribbon <-
+  ggproto(
+    "StatStepRibbon", Stat,
+    required_aes = c("x", "ymin", "ymax"),
+    compute_group = function(data, scales, direction="hv",
+                             yvars=c("ymin", "ymax"), ...) {
+      stairstepn(data=data, direction=direction, yvars=yvars)
+    }
+
+  )
+
+stairstepn <- function(data, direction="hv", yvars="y") {
+
+  direction <- match.arg(direction, c("hv", "vh"))
+
+  data <- as.data.frame(data)[order(data$x),]
+
+  n <- nrow(data)
+
+  if (direction == "vh") {
+    xs <- rep(1:n, each=2)[-2*n]
+    ys <- c(1, rep( 2:n, each=2))
+  } else {
+    ys <- rep(1:n, each=2)[-2*n]
+    xs <- c(1, rep(2:n, each=2))
+  }
+
+  data.frame(
+    x=data$x[xs],
+    data[ys, yvars, drop=FALSE],
+    data[xs, setdiff(names(data), c("x", yvars)), drop=FALSE]
+  )
+
+}
