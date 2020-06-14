cc44e7447b6424266c4e899704b6fe1fa32a6623
Marcio Augusto Diniz <dnz.marcio@gmail.com>
2020-06-13 19:42:19 -0700

    Update logistic regression.

--- R/logistic_regression.R
+++ R/logistic_regression.R
@@ -39,11 +39,10 @@
 #'@importFrom utils write.csv
 #'
 #'@export
-nt_simple_logistic <- function(data, response, ...,
+nt_simple_logistic <- function(data, response, ..., increment = NULL,
                                format = TRUE, digits = 2, digits.p = 3,
                                save = FALSE, file = "simple_logistic"){
 
-  data <- as_data_frame(data)
   response <- enquo(response)
   aux <- quos(...)
 
@@ -71,6 +70,7 @@ nt_simple_logistic <- function(data, response, ...,
   temp <- map2(.x = vars, .y = vars.name, .f = aux_simple_logistic,
                response = response, response.label = response.label,
                add = add, add.name = add.name, add.label = add.label,
+               increment = increment,
                format = format)
 
   out <- Reduce(rbind, temp)
@@ -81,9 +81,6 @@ nt_simple_logistic <- function(data, response, ...,
                           AIC = round(.data$AIC, digits),
                           BIC = round(.data$BIC, digits),
                           deviance = round(.data$deviance, digits)) %>%
-      replace_na(list(n = "", null.deviance = "", df.null = "",
-                      logLik = "", AIC = "", BIC = "",
-                      deviance = "", df.residual = "")) %>%
       transmute(Variable = .data$term, OR = .data$group,
                 `Estimate (95% CI)` = paste0(round(.data$estimate, digits), " (",
                                  round(.data$conf.low, digits), " ; ",
@@ -95,7 +92,12 @@ nt_simple_logistic <- function(data, response, ...,
                 n = .data$n, null.deviance = .data$null.deviance,
                 logLik = .data$logLik, AIC = .data$AIC, BIC = .data$BIC,
                 deviance = .data$deviance) %>%
-      replace_na(list(`Wald p value` = "", `LR p value` = ""))
+      mutate(`Estimate (95% CI)` =
+               recode(`Estimate (95% CI)`, `NA (NA ; NA)` = "Reference")) %>%
+      replace_na(list(`Wald p value` = "", `LR p value` = "",
+                      n = "", null.deviance = "", df.null = "",
+                      logLik = "", AIC = "", BIC = "",
+                      deviance = "", df.residual = ""))
   }
 
   if (save){
@@ -105,83 +107,90 @@ nt_simple_logistic <- function(data, response, ...,
   return(out)
 }
 
-#'@importFrom dplyr bind_cols rename
 aux_simple_logistic <- function(var, var.name, response, response.label,
-                                add, add.name, add.label,
+                                add, add.name, add.label, increment,
                                 format){
 
   var.label <- extract_label(var, var.name)
-  aux <- cbind(add, var)
-  aux.label <- c(add.label, var.label)
 
-  if(ncol(aux) > 1) {
-    var.class <- unlist(map(cbind(add, var), is.numeric))
+  if (!is.null(add)){
+    aux <- data.frame(add, var)
+    aux.labels <- c(add.label, var = var.label)
+    aux.names <- c(add.name, var.name)
+    data.model <- data.frame(response = response, add = add, var = var)
   } else {
-    var.class <- list(var = is.numeric(var))
+    aux <- data.frame(var)
+    aux.labels <- c(var = var.label)
+    aux.names <- var.name
+    data.model <- data.frame(response = response, var = var)
   }
 
   tab.labels <- list()
   tab.levels <- list()
 
-  for (i in 1:length(var.class)){
-    if (var.class[[i]]){
-      tab.labels[[colnames(aux)[i]]] <- aux.label[[i]]
-      tab.levels[[colnames(aux)[i]]] <- ""
+  for (i in 1:ncol(aux)){
+    if (is.numeric(aux[, i])){
+      tab.levels[[i]] <- ifelse(is.null(increment[[aux.names[[i]]]]),
+                              "every 1 unit of change",
+                              paste0("every ",
+                                     increment[[aux.names[i]]],
+                                     " unit of change"))
     } else {
-      tab.labels[[colnames(aux)[i]]] <- paste0(aux.label[[i]], ":")
       lv <- levels(var)
-      tab.levels[[colnames(aux)[i]]] <- paste0(lv[2:length(lv)], "/", lv[1])
+      tab.levels[[i]] <- paste0(lv[2:length(lv)], "/", lv[1])
     }
+
+    tab.labels[[names(aux.labels)[i]]] <- rep(aux.labels[[i]], length(tab.levels[[i]]))
   }
 
-  if (!is.list(tab.labels))
-    tab.labels <- setNames(as.list(tab.labels), "var")
+  out <- fit_logistic(data.model, tab.labels, tab.levels, var.label, increment)
 
-  data.model <- bind_cols(response = response, add = add, var = var)
-  out <- fit_logistic(data.model, tab.labels, tab.levels, var.label)
+  # if (any(duplicated(out$term))){
+  #   index <- which(duplicated(out$term, fromLast = TRUE))
+  #   out[(index+1):(index + length(lv) - 1), ] <- out[index:(index + length(lv) - 2), ]
+  #   out[index, 3:ncol(out)] <- NA
+  #   out[index:(index+length(lv)-1), 2] <- lv
+  # }
 
-  if (format)
-    out <- out %>%
-    mutate(p.value.lr = ifelse(duplicated(.data$term), NA, .data$p.value.lr),
-           term = ifelse(duplicated(.data$term), "", .data$term))
+  if (format){
+    out$p.value.lr = ifelse(duplicated(out$term), NA, out$p.value.lr)
+    out$term = ifelse(duplicated(out$term), "", out$term)
+  }
 
   return(out)
 }
 
 #'@importFrom broom tidy glance
-#'@importFrom tidyr separate replace_na
-#'@importFrom dplyr select transmute mutate bind_cols
-#'@importFrom stringr str_replace_all
 #'@importFrom stats na.exclude glm anova
-fit_logistic <- function(data, tab.labels, tab.levels, var.label){
+fit_logistic <- function(data, tab.labels, tab.levels, var.label, increment){
 
   data <- na.exclude(data)
 
+  if (!is.null(increment))
+    for (i in 1:length(increment)){
+      data[[tab.labels[[i]]]] <- data[[tab.labels[[i]]]]/increment[[i]]
+    }
+
   fit <- glm(response ~ ., data = data, family = "binomial")
 
-  temp <- tidy(fit, exponentiate = TRUE, conf.int = TRUE) %>%
-    mutate(term = str_replace_all(.data$term, unlist(tab.labels))) %>%
-    select(-.data$std.error, -.data$statistic) %>%
-    filter(.data$term != "(Intercept)") %>%
-    separate(.data$term, into = c("term", "group"), sep = ":", fill = "right") %>%
-    mutate(group = unlist(tab.levels))
-
+  temp <- tidy(fit, exponentiate = TRUE, conf.int = TRUE)
+  temp$term <- c("(Intercept)", unlist(tab.labels))
+  temp$group <- c("", unlist(tab.levels))
+  temp <- temp[-1, c(1, 8, 2, 6, 7, 5)]
 
   p.value.lr <- rep(NA, length(unique(temp$term)))
   for (i in 2:ncol(data)){
-
     if (ncol(data) > 2){
       fit0 <- glm(response ~ ., data = data[, -i], family = "binomial")
       p.value.lr[(i-1)] <- anova(fit0, fit, test = "Chisq")$`Pr(>Chi)`[2]
     } else {
-      fit0 <- glm(response ~ 1, data = data[, -i], family = "binomial")
+      fit0 <- glm(response ~ 1, data = data, family = "binomial")
       p.value.lr[(i-1)] <- anova(fit0, fit, test = "Chisq")$`Pr(>Chi)`[2]
     }
   }
 
-  aux01 <- bind_cols(term = unique(temp$term), p.value.lr = p.value.lr)
-  aux02 <- bind_cols(n = nrow(na.exclude(data)), glance(fit))
-
+  aux01 <- data.frame(term = unique(temp$term), p.value.lr = p.value.lr)
+  aux02 <- data.frame(n = nrow(data), glance(fit))
   aux <- merge(aux01, aux02, by = 0, all = TRUE)[-1]
 
   out <- merge(temp, aux, by = "term", all = TRUE)
@@ -224,11 +233,14 @@ fit_logistic <- function(data, tab.labels, tab.levels, var.label){
 #'@importFrom purrr map
 #'@importFrom utils write.csv
 #'@export
-nt_multiple_logistic <- function(fit, ci.type = "lr",
+nt_multiple_logistic <- function(fit, ci.type = "lr", user.contrast = NULL, user.contrast.interaction = NULL,
                                  format = TRUE, digits = 2, digits.p = 3,
                                  save = FALSE, file = "nt_multiple_logistic"){
 
-  out <- aux_multiple_logistic(fit, format, ci.type)
+  out <- aux_multiple_logistic(fit = fit, ci.type = ci.type,
+                               user.contrast = user.contrast,
+                               user.contrast.interaction = user.contrast.interaction,
+                               format = format)
   ref <- reference_df(fit)$ref
 
   if (format)
@@ -238,7 +250,7 @@ nt_multiple_logistic <- function(fit, ci.type = "lr",
                                            round(.data$conf.low, digits), " ; ",
                                            round(.data$conf.high, digits), ")"),
               `p value` = ifelse(round(.data$p.value, digits.p) == 0, "< 0.001",
-                                    as.character(round(.data$p.value, digits.p)))) %>%
+                                 as.character(round(.data$p.value, digits.p)))) %>%
     replace_na(list(`p value` = ""))
 
   if (save)
@@ -253,11 +265,13 @@ nt_multiple_logistic <- function(fit, ci.type = "lr",
 #'@importFrom stringr str_replace_all
 #'@importFrom tidyr separate
 #'@importFrom broom tidy
-aux_multiple_logistic <- function(fit, format, ci.type){
+aux_multiple_logistic <- function(fit, ci.type, user.contrast, user.contrast.interaction, format){
 
   aux <- extract_data(fit)
 
-  effect <- effect.glm(fit, type = ci.type, exponentiate = TRUE) %>%
+  effect <- effect.glm(fit, fit.vars = aux, type = ci.type,
+                       user.contrast = user.contrast,
+                       user.contrast.interaction = user.contrast.interaction) %>%
     mutate(term = str_replace_all(.data$term, unlist(aux$var.labels))) %>%
     separate(.data$term, into = c("variable", "or"), sep = ":")
 
@@ -281,9 +295,8 @@ aux_multiple_logistic <- function(fit, format, ci.type){
 }
 
 #'@importFrom stats model.matrix formula setNames anova vcov glm update.formula
-effect.glm <- function(fit, type, exponentiate){
+effect.glm <- function(fit, fit.vars, type, user.contrast, user.contrast.interaction){
 
-  aux <- extract_data(fit)
   ref <- reference_df(fit)$df
   beta <- as.numeric(fit$coefficients)
   beta.var <- as.matrix(vcov(fit))
@@ -291,23 +304,24 @@ effect.glm <- function(fit, type, exponentiate){
 
   interaction <- colnames(attr(fit$terms, "factors"))[attr(fit$terms, "order") > 1]
 
-  for (i in 1:length(aux$var)){
+  for (i in 1:length(fit.vars$var)){
 
     if (length(interaction) > 0){
-      cond.interaction <- grepl(aux$var[i], x = interaction, fixed = TRUE)
+      cond.interaction <- grepl(fit.vars$var[i], x = interaction, fixed = TRUE)
     } else {
       cond.interaction <- FALSE
     }
 
     if (all(!cond.interaction)){
-      temp <- contrast_df(aux$data, aux$var[i], ref)
+      temp <- contrast_df(data = fit.vars$data, var = fit.vars$var[i],
+                          ref = ref, user.contrast = user.contrast)
       design.matrix <- model.matrix(formula(fit), data = temp$new.data)
 
-      drop <- which(grepl(aux$var[i], x = as.character(term.labels), fixed = TRUE))
+      drop <- which(grepl(fit.vars$var[i], x = as.character(term.labels), fixed = TRUE))
       fit0 <- glm(update.formula(fit$formula,
                                  paste0(" ~ . - ", paste(term.labels[drop],
                                                          collapse = " - "))),
-                  data = aux$data, family = "binomial")
+                  data = na.exclude(fit.vars$data), family = "binomial")
 
       contrast <- contrast_calc(fit = fit, fit0 = fit0,
                                 design.matrix = design.matrix,
@@ -323,14 +337,19 @@ effect.glm <- function(fit, type, exponentiate){
 
     } else {
       for (k in which(cond.interaction)){
-        interaction.vars <- aux$var[sapply(aux$var, grepl, x = as.character(interaction[k]), fixed = TRUE)]
-        others <- interaction.vars[interaction.vars != aux$var[i]]
-        temp <- contrast_df(aux$data, aux$var[i], ref, others)
+
+        interaction.vars <- fit.vars$var[sapply(fit.vars$var, grepl, x = as.character(interaction[k]), fixed = TRUE)]
+        others <- interaction.vars[interaction.vars != fit.vars$var[i]]
+        temp <- contrast_df(data = fit.vars$data, var = fit.vars$var[i],
+                            ref = ref, user.contrast = user.contrast,
+                            interaction = others,
+                            user.contrast.interaction = user.contrast.interaction)
+
         design.matrix <- sapply(temp$new.data, function(x) model.matrix(fit, x), simplify = FALSE)
 
-        drop <- which(grepl(aux$var[i], x = as.character(term.labels), fixed = TRUE))
+        drop <- which(grepl(fit.vars$var[i], x = as.character(term.labels), fixed = TRUE))
         fit0 <- glm(update.formula(fit$formula, paste0(" ~ . - ", paste(term.labels[drop], collapse = " - "))),
-                    data = aux$data, family = "binomial")
+                    data = fit.vars$data, family = "binomial")
 
         contrast <- contrast_calc(fit = fit, fit0 = fit0, design.matrix = design.matrix,
                                   beta = beta, beta.var = beta.var,
@@ -347,13 +366,10 @@ effect.glm <- function(fit, type, exponentiate){
     }
   }
 
-  if (exponentiate)
-    out[, 2:4] <- exp(out[, 2:4])
-
+  out[, 2:4] <- exp(out[, 2:4])
   colnames(out) <- c("term", "estimate", "conf.low", "conf.high", "p.value")
   return(out)
 
 }
-
 
 
