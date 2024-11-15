3f25618f4ddf68b7fd1c5930f031f7ad34a7941f
Marcio Augusto Diniz <dnz.marcio@gmail.com>
2024-11-15 10:17:25 -0500

    Revamp glm functions

--- R/glm_regression.R
+++ R/glm_regression.R
@@ -49,9 +49,10 @@
 #'@export
 nt_simple_glm <- function(data, response, ...,
                           family, robust_variance = NULL,
-                          increment = NULL,
-                          ci_type = "Wald", conf_level = 0.95,
-                          exponentiate = FALSE,
+                          exponentiate = TRUE,
+                          type = "wald", contrast_qt = "one-unit",
+                          user_contrast = NULL,
+                          table_reference = TRUE,
                           format = TRUE, labels = NULL,
                           digits = 2, digits_p = 3,
                           save = FALSE, file = "simple_logistic"){
@@ -67,71 +68,86 @@ nt_simple_glm <- function(data, response, ...,
       vars <- select(.data = vars, -!!aux[[i]])
     }
     add <- select(.data = data, !!!aux)
-    add.name <- names(add)
-    add.label <- map2(add, add.name, extract_label)
+    add_name <- names(add)
+    add_label <- map2(add, add_name, extract_label)
   } else {
     add <- NULL
-    add.name <- NULL
-    add.label <- NULL
+    add_name <- NULL
+    add_label <- NULL
   }
 
-  vars.name <- names(vars)
-  response.name <- names(response)
+  vars_name <- names(vars)
+  response_name <- names(response)
 
   if (!is.null(labels)){
     vars <- data_labeller(vars, labels)
-    vars.label <- map2(.x = vars, .y = as.list(vars.name),
+    vars_label <- map2(.x = vars, .y = as.list(vars_name),
                        .f = extract_label)
 
+
     response <- data_labeller(response, labels)
-    response.label <- extract_label(response, response.name)
+    response_label <- extract_label(response, response_name)
+
   } else {
-    vars.label <- map2(.x = vars, .y = as.list(vars.name),
+    vars_label <- map2(.x = vars, .y = as.list(vars_name),
                        .f = extract_label)
   }
 
-  temp <- pmap(.l = list(vars, vars.name, vars.label),
+  temp <- pmap(.l = list(vars, vars_name, vars_label),
                .f = aux_simple_glm,
-               response = response[[1]], response.label = response.label,
-               add = add, add.name = add.name, add.label = add.label,
-               family = family, robust_variance = robust_variance,
-               increment = increment, exponentiate = exponentiate,
-               conf_level = conf_level, ci_type = ci_type,
+               response = response[[1]],
+               response_label = response_label,
+               add = add,
+               add_name = add_name,
+               add_label = add_label,
+               family = family,
+               exponentiate = exponentiate,
+               robust_variance = robust_variance,
+               type = type,
+               contrast_qt = contrast_qt,
+               user_contrast = user_contrast,
+               table_reference = table_reference,
                format = format)
 
   out <- Reduce(rbind, temp)
 
   if (format){
-    out <- out |> mutate(null.deviance = round(.data$null.deviance, digits),
-                          logLik = round(.data$logLik, digits),
-                          AIC = round(.data$AIC, digits),
-                          BIC = round(.data$BIC, digits),
-                          deviance = round(.data$deviance, digits)) |>
-      transmute(Variable = .data$term, Group = .data$group,
-                `Estimate (95% CI)` = ifelse(.data$estimate == 1 &
-                                              is.na(.data$conf.low) &
-                                              is.na(.data$conf.high),
+    out <- out |>
+      # mutate(null.deviance = round(.data$null.deviance, digits),
+      #                     logLik = round(.data$logLik, digits),
+      #                     AIC = round(.data$AIC, digits),
+      #                     BIC = round(.data$BIC, digits),
+      #                     deviance = round(.data$deviance, digits)) |>
+      transmute(Variable = .data$variable, Contrast = .data$group,
+                `Estimate (95% CI)` = ifelse(is.na(.data$estimate) &
+                                               is.na(.data$conf_low) &
+                                               is.na(.data$conf_high),
                                              "Reference",
-                                 paste0(round(.data$estimate, digits), " (",
-                                 round(.data$conf.low, digits), " ; ",
-                                 round(.data$conf.high, digits), ")")),
-                `Wald p value` = ifelse(round(.data$p.value, digits_p) == 0, "< 0.001",
-                                 as.character(round(.data$p.value, digits_p))),
-                `LR p value` = ifelse(round(.data$p.value.lr, digits_p) == 0, "< 0.001",
-                                        as.character(round(.data$p.value.lr, digits_p))),
-                n = as.character(.data$n),
-                null.deviance = as.character(.data$null.deviance),
-                logLik = as.character(.data$logLik),
-                AIC = as.character(.data$AIC),
-                BIC = as.character(.data$BIC),
-                deviance =as.character(.data$deviance)) |>
+                                             paste0(round(.data$estimate, digits), " (",
+                                                    round(.data$conf_low, digits), " ; ",
+                                                    round(.data$conf_high, digits), ")")),
+                `Wald p value` = ifelse(is.na(.data$p_value_wald), "",
+                                   ifelse(round(.data$p_value_wald, digits_p) == 0, "< 0.001",
+                                 as.character(round(.data$p_value_wald, digits_p)))),
+                `LR p value` = ifelse(is.na(.data$p_value_lr), "",
+                                        ifelse(round(.data$p_value_lr, digits_p) == 0, "< 0.001",
+                                               as.character(round(.data$p_value_lr, digits_p))))
+                # `LR p value` = ifelse(round(.data$p_value.lr, digits_p) == 0, "< 0.001",
+                #                         as.character(round(.data$p_value.lr, digits_p))),
+                # n = as.character(.data$n),
+                # null.deviance = as.character(.data$null.deviance),
+                # logLik = as.character(.data$logLik),
+                # AIC = as.character(.data$AIC),
+                # BIC = as.character(.data$BIC),
+                # deviance =as.character(.data$deviance)
+                ) |>
       mutate(`Estimate (95% CI)` =
                recode(.data$`Estimate (95% CI)`,
-                      `NA (NA ; NA)` = "Reference")) |>
-      replace_na(list(`Wald p value` = "", `LR p value` = "",
-                      n = "", null.deviance = "", df.null = "",
-                      logLik = "", AIC = "", BIC = "",
-                      deviance = "", df.residual = ""))
+                      `NA (NA ; NA)` = "Reference")) #|>
+      # replace_na(list(`Wald p value` = "", `LR p value` = "",
+      #                 n = "", null.deviance = "", df.null = "",
+      #                 logLik = "", AIC = "", BIC = "",
+      #                 deviance = "", df.residual = ""))
   }
 
   if (save){
@@ -141,12 +157,14 @@ nt_simple_glm <- function(data, response, ...,
   return(out)
 }
 
-aux_simple_glm <- function(var, var.name, var.label,
-                           response, response.label,
-                           add, add.name, add.label,
-                           family, robust_variance,
-                           increment, exponentiate,
-                           conf_level, ci_type,
+aux_simple_glm <- function(var, var_name, var_label,
+                           response, response_label,
+                           add, add_name, add_label,
+                           family,
+                           exponentiate, robust_variance,
+                           type, contrast_qt,
+                           user_contrast,
+                           table_reference,
                            format){
 
   if (is.factor(var)){
@@ -155,46 +173,34 @@ aux_simple_glm <- function(var, var.name, var.label,
 
   if (!is.null(add)){
     aux <- data.frame(add, var)
-    aux.labels <- c(add.label, var = var.label)
-    aux.names <- c(add.name, var.name)
-    data.model <- data.frame(response = response, add = add, var = var)
+    aux_labels <- c(add_label, var = var_label)
+    aux_names <- c(add_name, var_name)
+    data_model <- data.frame(response = response, add = add, var = var)
   } else {
     aux <- data.frame(var)
-    aux.labels <- c(var = var.label)
-    aux.names <- var.name
-    data.model <- data.frame(response = response, var = var)
+    aux_labels <- c(var = var_label)
+    aux_names <- var_name
+    data_model <- data.frame(response = response, var = var)
   }
 
-  tab.labels <- list()
-  tab.levels <- list()
 
-  for (i in 1:ncol(aux)){
-    if (is.numeric(aux[, i])){
-      tab.levels[[names(aux.labels)[i]]] <- ifelse(is.null(increment[[aux.names[[i]]]]),
-                              "every 1 unit of change",
-                              paste0("every ",
-                                     increment[[aux.names[i]]],
-                                     " unit of change"))
-    } else {
-      if (!is.factor(aux[, i]))
-        aux[, i] <- as.factor(aux[, i])
+  out <- fit_simple_glm(data_model, family,
+                        exponentiate, robust_variance,
+                        type, contrast_qt,
+                        user_contrast,
+                        table_reference,
+                        var_label)
 
-      lv <- levels(aux[, i])
-      tab.levels[[names(aux.labels)[i]]] <- lv[1:length(lv)] #paste0(lv[2:length(lv)], "/", lv[1])
-    }
+  out <- out |>
+    separate(.data$term, into = c("variable", "group"), sep = ":")
 
-    tab.labels[[names(aux.labels)[i]]] <- rep(aux.labels[[i]], length(tab.levels[[names(aux.labels)[i]]]))
-  }
-
-  out <- fit_simple_glm(data.model, family,
-                        tab.labels, tab.levels, var.label,
-                        robust_variance, increment[[var.name]],
-                        exponentiate, conf_level, ci_type)
-
-  if (format){
-    out$p.value.lr = ifelse(duplicated(out$term), NA, out$p.value.lr)
-    out$term = ifelse(duplicated(out$term), "", out$term)
-  }
+  if (format)
+    out <- out |>
+    group_by(.data$variable) |>
+    mutate(aux_variable = ifelse(duplicated(.data$variable), "", .data$variable)) |>
+    ungroup(.data$variable) |>
+    select(-.data$variable) |>
+    rename(variable = .data$aux_variable)
 
   return(out)
 }
@@ -203,65 +209,64 @@ aux_simple_glm <- function(var, var.name, var.label,
 #'@importFrom stats na.exclude glm anova
 #'@importFrom lmtest coeftest
 #'@importFrom stringr str_which
-fit_simple_glm <- function(data, family,
-                           tab.labels, tab.levels, var.label,
-                           robust_variance, increment,
-                           exponentiate, conf_level, ci_type){
+fit_simple_glm <- function(data_model, family,
+                           exponentiate, robust_variance,
+                           type, contrast_qt,
+                           user_contrast,
+                           table_reference,
+                           var_label){
 
-  data <- na.exclude(data)
+  data_model <- na.exclude(data_model)
 
-  if (!is.null(increment))
-      data[["var"]] <- data[["var"]]/increment
+  fit <- glm(response ~ ., data = data_model, family = family)
+  aux <- extract_data(fit)
 
-  fit <- glm(response ~ ., data = data, family = family)
+  ref <- reference_df(fit)$df
+  beta <- as.numeric(fit$coefficients)
+  beta_var <- as.matrix(vcov(fit))
+  term_labels <- attr(fit$terms, "term.labels")
 
-  if (is.null(robust_variance)){
-    temp <- tidy(fit, exponentiate = exponentiate,
-                 conf.level = conf_level,
-                 conf.type = ci_type,
-                 conf.int = TRUE)
+  for (i in 1:length(aux$var)){
 
-  } else {
-    step <- coeftest(fit, vcov. = robust_variance)
-    temp <- tidy(step, exponentiate = exponentiate,
-                 conf.level = conf_level,
-                 conf.int = TRUE)
+    temp <- contrast_df(data = aux$data,
+                        var = aux$var[i],
+                        ref = ref,
+                        contrast_qt = contrast_qt,
+                        user_contrast = user_contrast,
+                        table_reference = table_reference)
+    temp$label <- gsub("var:", paste0(var_label, ":"), temp$label)
+
+    design_matrix <- model.matrix(formula(fit), data = temp$new.data)
+
+    drop <- which(grepl(aux$var[i], x = as.character(term_labels), fixed = TRUE))
+    fit0 <- update(fit, paste("~ . -", term_labels[drop]))
+
+    contrast <- contrast_calc(fit = fit, fit0 = fit0,
+                              design_matrix = design_matrix,
+                              beta = beta, beta_var = beta_var,
+                              type = type)
+
+    label_index <- grep("every 1 unit of change", temp$label)
+    if (table_reference & length(label_index) == 0)
+      contrast <- rbind(NA, contrast)
+
+    temp <- data.frame(term = temp$label, contrast)
+
+    if (length(label_index) == 0)
+      temp[1:2, 6] <- temp[2:1, 6]
+
+    if (i > 1)
+      temp <- rbind(out, temp)
+
+    out <- temp
+
   }
-
-  nlv <- lapply(tab.levels, length)
-
-  for (i in 1:length(nlv)){
-    if (nlv[[i]] > 1){
-      add.row <- str_which(temp$term, names(tab.levels)[i])[1]
-      temp <- rbind(temp[1:(add.row-1), ], NA,
-                    temp[add.row:nrow(temp), ])
-      temp[add.row, 1] <- paste0(names(tab.levels)[i], tab.levels[[i]][1])
-      temp[add.row, 2] <- 1
-    }
-
-  }
-
-  temp$term <- c("(Intercept)", unlist(tab.labels))
-  temp$group <- c("", unlist(tab.levels))
-  temp <- temp[, c(1, 8, 2, 6, 7, 5)]
 
   if (exponentiate)
-    temp <- temp[-1, ]
+    out[, 2:4] <- exp(out[, 2:4])
 
-  p.value.lr <- rep(NA, length(unique(unlist(tab.labels))))
-
-  for (i in 2:ncol(data)){
-    if (ncol(data) > 2){
-      fit0 <- glm(response ~ ., data = data[, -i], family = family)
-      p.value.lr[(i-1)] <- anova(fit0, fit, test = "Chisq")$`Pr(>Chi)`[2]
-    } else {
-      fit0 <- glm(response ~ 1, data = data, family = family)
-      p.value.lr[(i-1)] <- anova(fit0, fit, test = "Chisq")$`Pr(>Chi)`[2]
-    }
-  }
-
-  aux <- data.frame(p.value.lr = p.value.lr, n = nrow(data), glance(fit))
-  out <- merge(temp, aux, by = 0, all = TRUE, sort = TRUE)[, -1]
+  rownames(out) <- NULL
+  colnames(out) <- c("term", "estimate", "conf_low", "conf_high", "p_value_wald", "p_value_lr")
 
   return(out)
 }
@@ -329,11 +334,14 @@ nt_multiple_glm <- function(fit, exponentiate = FALSE,
               `Estimate (95% CI)` = ifelse(is.na(.data$estimate),
                                            "Reference",
                                            paste0(round(.data$estimate, digits), " (",
-                                                  round(.data$conf.low, digits), " ; ",
-                                                  round(.data$conf.high, digits), ")")),
-              `p value` = ifelse(is.na(.data$p.value), "",
-                                 ifelse(round(.data$p.value, digits_p) == 0, "< 0.001",
-                                        as.character(round(.data$p.value, digits_p))))) |>
+                                                  round(.data$conf_low, digits), " ; ",
+                                                  round(.data$conf_high, digits), ")")),
+              `Wald p value` = ifelse(is.na(.data$p_value_wald), "",
+                                 ifelse(round(.data$p_value_wald, digits_p) == 0, "< 0.001",
+                                        as.character(round(.data$p_value_wald, digits_p)))),
+              `LR p value` = ifelse(is.na(.data$p_value_lr), "",
+                                      ifelse(round(.data$p_value_lr, digits_p) == 0, "< 0.001",
+                                             as.character(round(.data$p_value_lr, digits_p))))) |>
     replace_na(list(`p value` = ""))
 
   if (!is.null(labels)){
@@ -348,7 +356,7 @@ nt_multiple_glm <- function(fit, exponentiate = FALSE,
   if (save)
     write.csv(out$effect, file = paste0(file, ".csv"))
 
-  out <- list(effect = out$effect, coef = out$coef, ref = ref)
+  out <- list(effect = out$effect, output = out$coef, ref = ref)
 
   return(out)
 }
@@ -376,21 +384,17 @@ aux_multiple_glm <- function(fit, exponentiate, robust_variance,
     separate(.data$term, into = c("variable", "group"), sep = ":")
 
   if (format)
-    effect <- effect |> group_by(.data$variable) |>
+    effect <- effect |>
+    group_by(.data$variable) |>
     mutate(aux_variable = ifelse(duplicated(.data$variable), "", .data$variable)) |>
-    ungroup(.data$variable) |> select(-.data$variable) |>
+    ungroup(.data$variable) |>
+    select(-.data$variable) |>
     rename(variable = .data$aux_variable)
 
-  # temp <- unlist(aux$var.labels)
-  # labels <- paste0(temp, " ")
-  # names(labels) <- names(temp)
-  # coef <- tidy(fit) |>
-  #   mutate(term = str_replace_all(.data$term, labels),
-  #          term = sub(" $", "", x = .data$term))
+  output <- summary(fit)
+  statistics <- glance(fit)
 
-  coef <- summary(fit)
-
-  out <- list(effect = effect, coef = coef)
+  out <- list(effect = effect, output = output)
 
   return(out)
 }
@@ -436,13 +440,13 @@ fit_multiple_glm <- function(fit, fit_vars, exponentiate, robust_variance,
                                 beta = beta, beta_var = beta_var,
                                 type = type)
 
-      aux <- grep("one-unit change", temp$label)
-      if (table_reference & length(aux) == 0)
+      label_index <- grep("every 1 unit of change", temp$label)
+      if (table_reference & length(label_index) == 0)
         contrast <- rbind(NA, contrast)
 
       temp <- data.frame(term = temp$label, contrast)
 
-      if (type == "lr")
+      if (type == "lr"& length(label_index) == 0)
         temp[1:2, 5] <- temp[2:1, 5]
 
       if (i > 1)
@@ -479,7 +483,7 @@ fit_multiple_glm <- function(fit, fit_vars, exponentiate, robust_variance,
 
             if (j %in% temp$seq_nl){
               aux.contrast[[j]] <- matrix(NA, ncol = ncol(contrast))
-              colnames(aux.contrast[[j]]) <- c("estimate", "lower", "upper", "p.value")
+              colnames(aux.contrast[[j]]) <- c("estimate", "lower", "upper", "p_value")
             } else {
               aux.contrast[[j]] <- contrast[index, ]
               index <- index + 1
@@ -504,7 +508,7 @@ fit_multiple_glm <- function(fit, fit_vars, exponentiate, robust_variance,
   if (exponentiate)
     out[, 2:4] <- exp(out[, 2:4])
   rownames(out) <- NULL
-  colnames(out) <- c("term", "estimate", "conf.low", "conf.high", "p.value")
+  colnames(out) <- c("term", "estimate", "conf_low", "conf_high", "p_value_wald", "p_value_lr")
   return(out)
 
 }
