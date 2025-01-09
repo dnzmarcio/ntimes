ca635ada795a0c9e7399713022b6ec04c7f52a52
Marcio Augusto Diniz <dnz.marcio@gmail.com>
2025-01-09 16:50:44 -0500

    Debug

--- R/cox_regression.R
+++ R/cox_regression.R
@@ -514,7 +514,7 @@ fit_multiple_cox <- function(fit, fit_vars, type, contrast_qt,
 
       temp <- data.frame(term = temp$label, contrast)
 
-      if (length(label_index) == 0)
+      if (table_reference & length(label_index) == 0)
         temp[1:2, 6] <- temp[2:1, 6]
 
       if (i > 1)
