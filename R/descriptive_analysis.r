2a40c3482ef137ed65a8d0937f95fe3e2029e9fb
Marcio Augusto Diniz <dnz.marcio@gmail.com>
2026-04-14 13:02:56 -0400

    Add stratified descriptive analysis

--- R/descriptive_analysis.r
+++ R/descriptive_analysis.r
@@ -5,6 +5,7 @@
 #'
 #'@param data a data frame with the variables.
 #'@param group a character value indicating the group variable.
+#'@param strata a character value indicating the strata variable.
 #'@param labels a list of labels with components given by their variable names.
 #'@param measures_qt a list of functions to summarise quantitative variables. See more in details.
 #'@param measures_ql a list of functions to summarise qualitative variables. See more in details.
@@ -18,9 +19,10 @@
 #'functions \code{\link[ntimes]{helper_mean_sd}}, \code{\link[ntimes]{helper_median_iqr}},
 #'\code{\link[ntimes]{helper_median_range}} and \code{\link[ntimes]{helper_missing}}.
 #'For qualitative variables, percentage (frequency) is calculated using
-#'\code{\link[ntimes]{helper_perc_count}}.
+#'\code{\link[ntimes]{helper_perc_count}} or \code{\link[ntimes]{helper_count_perc}}.
 #'
 #'@return a data frame with summary for all variables by group.
+#'When \code{strata} is provided, a list of data frame with summarized statistics is the output.
 #'
 #'@examples
 #'data(iris)
@@ -32,7 +34,7 @@
 #'                                   Petal.Width = "Petal Width"))
 #'
 #'@importFrom purrr pmap
-#'@importFrom dplyr filter select
+#'@importFrom dplyr filter select bind_rows
 #'@importFrom utils write.csv
 #'@importFrom rlang := .data quo_is_null enquo
 #'@importFrom tibble tibble
@@ -40,6 +42,7 @@
 #'@export
 nt_describe <- function(data,
                         group = NULL,
+                        strata = NULL,
                         labels = NULL,
                         measures_qt = list(helper_mean_sd,
                                            helper_median_iqr,
@@ -51,50 +54,147 @@ nt_describe <- function(data,
                         file = "descriptive_analysis",
                         ...){
 
-  group <- enquo(group)
+  group.qs <- enquo(group)
+  strata.qs <- enquo(strata)
+  vars <- data
 
-  if (!quo_is_null(group)){
-    vars <- select(.data = data, -!!group)
-    group <- select(.data = data, !!group)
+  if (!quo_is_null(group.qs)){
+    group <- select(.data = data, !!group.qs)
     group.name <- names(group)
+
+    if(!is.factor(group[[1]])){
+      group[[1]] <- as.factor(group[[1]])
+      warning(paste0(group.name, " was transformed into as a factor."))
+    }
+
   } else {
-    vars <- data
     group <- NULL
     group.name <- NULL
   }
 
-  vars.name <- names(vars)
-  if (!is.null(labels)){
-    vars <- data_labeller(vars, labels)
-    vars.label <- map2(.x = vars, .y = as.list(vars.name),
-                       .f = extract_label)
-    if (!is.null(group)){
-      group <- data_labeller(group, labels)
-      group.label <- extract_label(group[[1]], group.name)
+  if (!quo_is_null(strata.qs)){
+    strata <- select(.data = data, !!strata.qs)
+    strata.name <- names(strata)
+
+    if(!is.factor(strata[[1]])){
+      strata[[1]] <- as.factor(strata[[1]])
+      warning(paste0(strata.name, " was transformed into as a factor."))
     }
+
   } else {
-    vars.label <- map2(.x = vars, .y = as.list(vars.name),
-                       .f = extract_label)
-    if (!is.null(group))
-      group.label <- extract_label(group[[1]], group.name)
+    strata <- NULL
+    strata.name <- NULL
   }
 
-  temp <- pmap(.l = list(vars, vars.name, vars.label),
-               .f = aux_describe,
-               group = group[[1]],
-               group.name = group.name,
-               group.label = group.label,
-               digits = digits,
-               measures_qt = measures_qt,
-               measures_ql = measures_ql,
-               ...)
+  if (is.null(strata)){
+    vars <- select(.data = data, -!!group.qs)
 
-  out <- Reduce(rbind, temp)
+    vars.name <- names(vars)
+    if (!is.null(labels)){
+      vars <- data_labeller(vars, labels)
+      vars.label <- map2(.x = vars, .y = as.list(vars.name),
+                         .f = extract_label)
 
-  if (save)
-    write.csv(out, file = paste0(file, ".csv"))
+      if (!is.null(group)){
+        group <- data_labeller(group, labels)
+        group.label <- extract_label(group[[1]], group.name)
+      }
+      if (!is.null(strata)){
+        strata <- data_labeller(strata, labels)
+        strata.label <- extract_label(strata[[1]], strata.name)
+      }
 
-  attr(out, "ntimes") <- "descriptive"
+    } else {
+      vars.label <- map2(.x = vars, .y = as.list(vars.name),
+                         .f = extract_label)
+      if (!is.null(group))
+        group.label <- extract_label(group[[1]], group.name)
+
+      if (!is.null(strata))
+        strata.label <- extract_label(strata[[1]], strata.name)
+    }
+
+    temp <- pmap(.l = list(vars, vars.name, vars.label),
+                 .f = aux_describe,
+                 group = group[[1]],
+                 group.name = group.name,
+                 group.label = group.label,
+                 digits = digits,
+                 measures_qt = measures_qt,
+                 measures_ql = measures_ql,
+                 ...)
+
+    out <- Reduce(rbind, temp)
+
+    if (save)
+      write.csv(out, file = paste0(file, ".csv"))
+
+    attr(out, "ntimes") <- "descriptive"
+
+  } else {
+    ls <- levels(strata[[1]])
+    ns <- length(ls)
+    out <- list()
+
+    for (i in 1:ns){
+
+      vars.strata <- vars |> filter(!!strata.qs == ls[i]) |>
+        select(-!!strata.qs, -!!group.qs)
+      group.strata <- vars |> select(!!group.qs, !!strata.qs) |>
+        filter(!!strata == ls[i]) |>
+        select(-!!strata.qs)
+
+      vars.name <- names(vars.strata)
+      if (!is.null(labels)){
+        vars <- data_labeller(vars, labels)
+        vars.label <- map2(.x = vars.strata, .y = as.list(vars.name),
+                           .f = extract_label)
+
+        if (!is.null(group)){
+          group <- data_labeller(group, labels)
+          group.label <- extract_label(group[[1]], group.name)
+        }
+        if (!is.null(strata)){
+          strata <- data_labeller(strata, labels)
+          strata.label <- extract_label(strata[[1]], strata.name)
+        }
+
+      } else {
+        vars.label <- map2(.x = vars.strata, .y = as.list(vars.name),
+                           .f = extract_label)
+        if (!is.null(group))
+          group.label <- extract_label(group[[1]], group.name)
+
+        if (!is.null(strata))
+          strata.label <- extract_label(strata[[1]], strata.name)
+      }
+
+      temp <- pmap(.l = list(vars.strata, vars.name, vars.label),
+                       .f = aux_describe,
+                       group = group.strata[[1]],
+                       group.name = group.name,
+                       group.label = group.label,
+                       digits = digits,
+                       measures_qt = measures_qt,
+                       measures_ql = measures_ql,
+                       ...)
+
+      out[[paste(ls[i])]] <- Reduce(rbind, temp)
+
+      if (save){
+        write.csv(bind_rows(out), file = paste0(file, ".csv"))
+      }
+
+
+      attr(out, "ntimes") <- "descriptive.stratified"
+    }
+
+
+  }
+
+
+
+
   return(out)
 }
 
